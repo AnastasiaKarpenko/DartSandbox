5ca2f509a3b586d2f5cc211a3fb55e2e92887a76
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-09 14:51:54 +0200

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,20 @@
+
+main(List<String> arguments) {
+
+  //Booleans
+
+  bool isOn; //this is a variable, it is gonna change over time
+  print('isOn = ${isOn}');
+
+  isOn = true;
+  print('isOn = ${isOn}');
+
+  isOn = false;
+  print('isOn = ${isOn}');
+
+  isOn = true;
+  print('isOn is a  ${isOn.runtimeType}');
+
+  
+
+}
---
+++ test/DartSandbox_test.dart
@@ -0,0 +1,8 @@
+import 'package:DartSandbox/DartSandbox.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
