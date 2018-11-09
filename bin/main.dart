a02a5c98205a2fe131dcbd281e175931a9c9150e
Anastasia Karpenko <morganaloca@gmail.com>
2018-11-09 14:53:16 +0200

    Do some math

--- bin/main.dart
+++ bin/main.dart
@@ -30,5 +30,8 @@ main(List<String> arguments) {
   int err = int.parse('12.09', onError: (source) => 0);
   print('err = ${err}');
 
-
+  //math
+  int dogyears = 7;
+  int dogage = age * dogyears;
+  print('Your age in dog years is: ${dogage}');
 }
